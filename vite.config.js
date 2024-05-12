67c19c244da677a99c0a3444398ff31bd9cec5cf
Catherine <41663485+catherineagl@users.noreply.github.com>
2024-05-12 13:54:18 -0400

    confir changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/3D-art-gallery',
   build: {
     rollupOptions: {
-      input: '/main.js',
+      input: {
+        main: './index.html',
+      },
     },
   },
 });
